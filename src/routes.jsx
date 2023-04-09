ba34a6ed172374a95618ece7b35c6d668be7fcc3
VitorMendonca62 <vitorqueiroz325@gmail.com>
2023-04-09 15:14:32 -0300

    .

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // PAGES
 import WhoAreYou from './pages/WhoAreYou';
 import Film from './pages/Film';
 
+const Home = () => {
+  return <Link to="/quem_e_voce">HOME</Link>;
+};
+
 export default function routes() {
-  
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/quem_e_voce" element={<WhoAreYou />} />
         <Route path="/filme" element={<Film />} />
       </Routes>
