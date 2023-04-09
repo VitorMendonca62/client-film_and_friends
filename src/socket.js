dbbed1479e151b98a0826d7aac17c1855c23696e
VitorMendonca62 <vitorqueiroz325@gmail.com>
2023-04-09 16:02:44 -0300

    .

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // const URL = 'http://localhost:4004';
-const URL = 'https://film-and-filmds.onrender.com:4004';
+const URL = 'https://film-and-filmds.onrender.com';
 
 export const socket = io(URL);
