d3b791dabc589ffa06f834165c2621f0e7793286
VitorMendonca62 <vitorqueiroz325@gmail.com>
2023-04-10 15:25:22 -0300

    .

--- src/socket.js
+++ src/socket.js
@@ -2,5 +2,5 @@ import { io } from 'socket.io-client';
 
 const URL = 'https://film-and-filmds.onrender.com';
 
-export const socket = io(URL, { port: 4004 });
+export const socket = io(URL, { port: 4004, });
 console.log(socket);
