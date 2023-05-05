6f05269994b698ba7f945ba1f5f3dcb5a5004552
VitorMendonca62 <vitorqueiroz325@gmail.com>
2023-05-05 17:25:41 -0300

    erro localhost

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-// const URL = 'https://film-and-filmds.onrender.com';
-const URL = 'http://localhost:4004';
+const URL = 'https://film-and-filmds.onrender.com';
+// const URL = 'http://localhost:4004';
 
 export const socket = io(URL, { transports: ['websocket'] });
